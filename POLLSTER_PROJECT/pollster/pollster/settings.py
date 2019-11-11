319c3222670c5233919ea5acc0731e4f2ddb3d71
Fredaya <56237603+Fredaya@users.noreply.github.com>
2019-11-11 23:45:38 +0100

    Update settings.py

--- POLLSTER_PROJECT/pollster/pollster/settings.py
+++ POLLSTER_PROJECT/pollster/pollster/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +121,5 @@ STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
